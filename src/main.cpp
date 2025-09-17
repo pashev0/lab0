4095503567395d35f8767f2311de1d4f3c450815
pashev0 <pashevalexander135790@gmail.com>
2025-09-17 22:16:58 +0300

    init new repository and add main

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    char sym;
+    std::cout << u8"Hello World!\n";
+    std::cout << u8"Введите символ:";
+    std::cin >> sym;
+
+    return 0;
+} 
